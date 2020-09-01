f677c6713c5144e9af84e4f6955ea09d7e99fa4b
WatsonD25 <inespo2016chen@gmail.com>
2020-09-01 13:23:22 +0800

    建立台南淹水6h_150mm

--- map/environment_map.py
+++ map/environment_map.py
@@ -43,7 +43,27 @@ soil.add_child(folium.GeoJson(
 ))
 
 #flood
-
+flood_tainan_6h_150mm=folium.GeoJson(
+    "flood_tainan_6h_150mm/geojson_format_flood1_6150.json",
+    name="flood_tainan_6h_150mm",
+    style_function=lambda x : {"stroke":False,"fillColor":"#ffd600","fillOpacity":0.75}
+).add_to(main_map)
+flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    "flood_tainan_6h_150mm/geojson_format_flood2_6150.json",
+    name="flood_tainan_6h_150mm",
+    style_function=lambda x : {"stroke":False,"fillColor":"#ffab00","fillOpacity":0.75}
+))
+flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    "flood_tainan_6h_150mm/geojson_format_flood3_6150.json",
+    name="flood_tainan_6h_150mm",
+    style_function=lambda x : {"stroke":False,"fillColor":"#ff6d00","fillOpacity":0.75}
+))
+flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    "flood_tainan_6h_150mm/geojson_format_flood4_6150.json",
+    name="flood_tainan_6h_150mm",
+    style_function=lambda x : {"stroke":False,"fillColor":"#dd2c00","fillOpacity":0.75}
+))
+flood_tainan_6h_150mm
 
 folium.LayerControl().add_to(main_map)
 main_map.save("map_for_environment.html")
