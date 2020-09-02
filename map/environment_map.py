275d05d6c927a290acf3733ba79c20e58a8b1a9b
WatsonD25 <inespo2016chen@gmail.com>
2020-09-02 09:18:29 +0800

    新增修改好的台南土壤液化到environment map

--- map/environment_map.py
+++ map/environment_map.py
@@ -1,7 +1,7 @@
 import folium
 # from folium import plugins
 
-main_map = folium.Map(location=[23.000909, 120.223148],zoom_start=12)
+main_map = folium.Map(location=[23.000909, 120.223148],zoom_start=15)
 
 
 # 將地圖訂位到user的位置
@@ -27,40 +27,45 @@ a.add_child(folium.GeoJson(
 
 #soil
 soil=folium.GeoJson(
-    "geojson_format_green_tainan.json",
+    "soil_tainan_json/geojson_format_green_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"green","fillOpacity":0.7}
 ).add_to(main_map)
 soil.add_child(folium.GeoJson(
-    "geojson_format_yellow_tainan.json",
+    "soil_tainan_json/geojson_format_yellow_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"yellow","fillOpacity":0.7}
 ))
 soil.add_child(folium.GeoJson(
-    "geojson_format_red_tainan.json",
+    "soil_tainan_json/geojson_format_red_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"red","fillOpacity":0.7}
 ))
 
+#台南市東區大學路1號
+tooltip='點擊獲取更多資訊'
+popup=folium.Popup(html='<b>台南市東區大學路1號</b>',max_width=200)
+folium.Marker(location=[22.996319990137735,120.21953003491251],tooktip=tooltip,popup=popup,icon=folium.Icon(color='red')).add_to(main_map)
+
 #flood
 flood_tainan_6h_150mm=folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood1_6150.json",
-    name="flood_tainan_6h_150mm",
+    name="6小時內下雨150mm之淹水區域",
     style_function=lambda x : {"stroke":False,"fillColor":"#ffd600","fillOpacity":0.75}
 ).add_to(main_map)
 flood_tainan_6h_150mm.add_child(folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood2_6150.json",
-    name="flood_tainan_6h_150mm",
+    name="6小時內下雨150mm之淹水區域",
     style_function=lambda x : {"stroke":False,"fillColor":"#ffab00","fillOpacity":0.75}
 ))
 flood_tainan_6h_150mm.add_child(folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood3_6150.json",
-    name="flood_tainan_6h_150mm",
+    name="6小時內下雨150mm之淹水區域",
     style_function=lambda x : {"stroke":False,"fillColor":"#ff6d00","fillOpacity":0.75}
 ))
 flood_tainan_6h_150mm.add_child(folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood4_6150.json",
-    name="flood_tainan_6h_150mm",
+    name="6小時內下雨150mm之淹水區域",
     style_function=lambda x : {"stroke":False,"fillColor":"#dd2c00","fillOpacity":0.75}
 ))
 
