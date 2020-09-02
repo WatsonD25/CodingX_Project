864786ce243a67608f5594d45bb9bfef7cf0494a
WatsonD25 <inespo2016chen@gmail.com>
2020-09-02 10:01:50 +0800

    修改台南土壤液化tooltip，新增台南6h_150mm的tooltip

--- map/environment_map.py
+++ map/environment_map.py
@@ -26,51 +26,59 @@ a.add_child(folium.GeoJson(
 
 
 #soil
-soil=folium.GeoJson(
+soil=folium.FeatureGroup('土壤液化')
+folium.GeoJson(
     "soil_tainan_json/geojson_format_green_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"green","fillOpacity":0.7},
     tooltip='低潛勢區'
-).add_to(main_map)
-soil.add_child(folium.GeoJson(
+).add_to(soil)
+folium.GeoJson(
     "soil_tainan_json/geojson_format_yellow_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"yellow","fillOpacity":0.7},
     tooltip='中潛勢區'
-))
-soil.add_child(folium.GeoJson(
+).add_to(soil)
+folium.GeoJson(
     "soil_tainan_json/geojson_format_red_tainan.json",
     name="土壤液化",
     style_function=lambda x : {"stroke":False,"fillColor":"red","fillOpacity":0.7},
     tooltip='高潛勢區'
-))
+).add_to(soil)
+soil.add_to(main_map)
 
 #台南市東區大學路1號
 tooltip='點擊獲取更多資訊'
 popup=folium.Popup(html='<b>台南市東區大學路1號</b>',max_width=200)
-folium.Marker(location=[22.996319990137735,120.21953003491251],tooktip=tooltip,popup=popup,icon=folium.Icon(color='red')).add_to(main_map)
+folium.Marker(location=[22.996319990137735,120.21953003491251],tooltip=tooltip,popup=popup,icon=folium.Icon(color='red')).add_to(main_map)
 
 #flood
-flood_tainan_6h_150mm=folium.GeoJson(
+flood_tainan_6h_150mm=folium.FeatureGroup('6小時內下雨150mm之淹水區域')
+folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood1_6150.json",
     name="6小時內下雨150mm之淹水區域",
-    style_function=lambda x : {"stroke":False,"fillColor":"#1e88e5","fillOpacity":0.75}
-).add_to(main_map)
-flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    style_function=lambda x : {"stroke":False,"fillColor":"#1e88e5","fillOpacity":0.75},
+    tooltip='淹水深度：0.5~1.0 公尺'
+).add_to(flood_tainan_6h_150mm)
+folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood2_6150.json",
     name="6小時內下雨150mm之淹水區域",
-    style_function=lambda x : {"stroke":False,"fillColor":"#1565c0","fillOpacity":0.75}
-))
-flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    style_function=lambda x : {"stroke":False,"fillColor":"#1565c0","fillOpacity":0.75},
+    tooltip='淹水深度：1.0~2.0 公尺'
+).add_to(flood_tainan_6h_150mm)
+folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood3_6150.json",
     name="6小時內下雨150mm之淹水區域",
-    style_function=lambda x : {"stroke":False,"fillColor":"#0d47a1","fillOpacity":0.75}
-))
-flood_tainan_6h_150mm.add_child(folium.GeoJson(
+    style_function=lambda x : {"stroke":False,"fillColor":"#0d47a1","fillOpacity":0.75},
+    tooltip='淹水深度：2.0~3.0 公尺'
+).add_to(flood_tainan_6h_150mm)
+folium.GeoJson(
     "flood_tainan_6h_150mm/geojson_format_flood4_6150.json",
     name="6小時內下雨150mm之淹水區域",
-    style_function=lambda x : {"stroke":False,"fillColor":"#022a66","fillOpacity":0.75}
-))
+    style_function=lambda x : {"stroke":False,"fillColor":"#022a66","fillOpacity":0.75},
+    tooltip='淹水深度：>3.0 公尺'
+).add_to(flood_tainan_6h_150mm)
+flood_tainan_6h_150mm.add_to(main_map)
 
 
 folium.LayerControl().add_to(main_map)
